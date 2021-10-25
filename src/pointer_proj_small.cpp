06e8dbfcae079e204e81a575eade8729fd2eaf1c
Keith Perkins <keith.perkins@cnu.edu>
2021-10-25 17:01:02 -0400

    extra comments

--- src/pointer_proj_small.cpp
+++ src/pointer_proj_small.cpp
@@ -9,11 +9,23 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
-#include "../includes1/constants.h"
-#include "../includes1/stringmanip.h"
-#include "../includes1/fileio.h"
+#include "../includes/constants.h"
+#include "../includes/stringmanip.h"
+#include "../includes/fileio.h"
 
 
 int main(int argc, char *argv[]) {	
-//TODO add your content here
+	//TODO Get commandline arguments
+	
+	//TODO can we read file? Fail if not
+	
+	//TODO how many tags found in file?
+	
+	//TODO how much memory to allocate?
+	
+	//TODO allocate memory
+	
+	//TODO replace original tag with new tags
+	
+	//TODO deallocate any memory allocated
 }
